d88d7540081f724839186f418c605403e5c02afb
Bruno Garutti <garuttibruno03@gmail.com>
2025-09-10 19:59:54 -0300

    Update Projeto1.c

--- Projeto1.c
+++ Projeto1.c
@@ -1,7 +1,7 @@
 /*Projeto 1 - Algoritmo e Programação II
 
 Grupo:
-
+ oi 
 - Klaus Gustavo Castelein Ricieri Moura - 10735808
 - Bruno Matheus Garutti Pinto - 10736747
 
@@ -44,4 +44,5 @@ int main(){
     for(int i=0; i<grau; i++){
         printf("X = %d ", x[i]);
     }
+
 }
