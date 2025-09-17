8dde3aab5d4c537588cca077ec957fb89e7f688c
KGCRM5 <klausgustavo123@gmail.com>
2025-09-17 02:10:48 -0300

    Variáveis somente INT

--- Projeto1.c
+++ Projeto1.c
@@ -11,18 +11,18 @@ Grupo:
 
 //Chamada das funções declaradas depois da main
 void menu();
-void lerPolinomio(double coeficientes[], int expoentes[], int *grau);
-void imprimirPolinomio(double coeficientes[], int expoentes[], int grau);
-double calcularValor(double coeficientes[], int expoentes[], int grau, double x);
-void somarPolinomios(double coeficientes1[], int expoentes1[], int grau1, double coeficientes2[], int expoentes2[], int grau2, double resultadoCoeficientes[], int resultadoExpoentes[], int *resultadoGrau);
-void multiplicarPolinomios(double coeficientes1[], int expoentes1[], int grau1, double coeficientes2[], int expoentes2[], int grau2, double resultadoCoeficientes[], int resultadoExpoentes[], int *resultadoGrau);
+void lerPolinomio(int coeficientes[], int expoentes[], int *grau);
+void imprimirPolinomio(int coeficientes[], int expoentes[], int grau);
+int calcularValor(int coeficientes[], int expoentes[], int grau, int x);
+void somarPolinomios(int coeficientes1[], int expoentes1[], int grau1, int coeficientes2[], int expoentes2[], int grau2, int resultadoCoeficientes[], int resultadoExpoentes[], int *resultadoGrau);
+void multiplicarPolinomios(int coeficientes1[], int expoentes1[], int grau1, int coeficientes2[], int expoentes2[], int grau2, int resultadoCoeficientes[], int resultadoExpoentes[], int *resultadoGrau);
 
 int main() {
     int opcao;
-    double p1_coefs[10], p2_coefs[10], resultado_coefs[20];
+    int p1_coefs[10], p2_coefs[10], resultado_coefs[20];
     int p1_exps[10], p2_exps[10], resultado_exps[20];
     int p1_grau, p2_grau, resultado_grau;
-    double x, valor;
+    int x, valor;
 
     //Chama o menu ao menos uma vez, até que o usuário escolha sair (opção 0)
     do {
@@ -34,9 +34,9 @@ int main() {
             printf("\n--- Calcular Valor de um Polinomio ---\n");
             lerPolinomio(p1_coefs, p1_exps, &p1_grau);
             printf("Digite o valor de x: ");
-            scanf("%lf", &x);
+            scanf("%d", &x);
             valor = calcularValor(p1_coefs, p1_exps, p1_grau, x);
-            printf("\nO valor do polinomio P(x) e: %.2lf\n", valor);
+            printf("\nO valor do polinomio P(x) e: %.2d\n", valor);
 
         } else if (opcao == 2) {
             printf("\n--- Somar Polinomios ---\n");
@@ -80,7 +80,7 @@ void menu() {
     printf("0. Sair\n");
 }
 
-void lerPolinomio(double coeficientes[], int expoentes[], int *grau) {
+void lerPolinomio(int coeficientes[], int expoentes[], int *grau) {
     int i;
     printf("Digite o grau do polinomio (maximo 10): ");
     scanf("%d", grau);
@@ -92,27 +92,27 @@ void lerPolinomio(double coeficientes[], int expoentes[], int *grau) {
 
     for (i = *grau; i >= 0; i--) {
         printf("Digite o coeficiente de x^%d: ", i);
-        scanf("%lf", &coeficientes[i]);
+        scanf("%d", &coeficientes[i]);
         expoentes[i] = i;
     }
 }
 
-void imprimirPolinomio(double coeficientes[], int expoentes[], int grau) {
+void imprimirPolinomio(int coeficientes[], int expoentes[], int grau) {
     int i;
     printf("P(x) = ");
     int primeiroTermo = 1; //Garante que não tenha um sinal de soma antes do primeiro termo
     
     for (i = grau; i >= 0; i--) {
         if (coeficientes[i] != 0) {
-            if (!primeiroTermo && coeficientes[i] > 0) {
+            if (!primeiroTermo && coeficientes[i] > 0) { //Para os termos após o primeiro
                 printf(" + ");
             }
             if (expoentes[i] > 1) {
-                printf("%.0lfx^%d", coeficientes[i], expoentes[i]);
+                printf("%dx^%d ", coeficientes[i], expoentes[i]);
             } else if (expoentes[i] == 1) {
                 printf("x", coeficientes[i]);
-            } else {
-                printf("%.0lf", coeficientes[i]);
+            } else { //Para o termo independente
+                printf("%d", coeficientes[i]);
             }
             primeiroTermo = 0;
         }
@@ -120,8 +120,8 @@ void imprimirPolinomio(double coeficientes[], int expoentes[], int grau) {
     printf("\n");
 }
 
-double calcularValor(double coeficientes[], int expoentes[], int grau, double x) {
-    double resultado = 0.0;
+int calcularValor(int coeficientes[], int expoentes[], int grau, int x) {
+    int resultado = 0;
     int i;
     for (i = 0; i <= grau; i++) {
         resultado += coeficientes[i] * pow(x, expoentes[i]);
@@ -129,7 +129,7 @@ double calcularValor(double coeficientes[], int expoentes[], int grau, double x)
     return resultado;
 }
 
-void somarPolinomios(double coeficientes1[], int expoentes1[], int grau1, double coeficientes2[], int expoentes2[], int grau2, double resultadoCoeficientes[], int resultadoExpoentes[], int *resultadoGrau) {
+void somarPolinomios(int coeficientes1[], int expoentes1[], int grau1, int coeficientes2[], int expoentes2[], int grau2, int resultadoCoeficientes[], int resultadoExpoentes[], int *resultadoGrau) {
     int i;
 
     //Utiliza o polinomio de maior grau como referência para o resultado
@@ -140,7 +140,7 @@ void somarPolinomios(double coeficientes1[], int expoentes1[], int grau1, double
     }
 
     for (i = 0; i <= *resultadoGrau; i++) {
-        resultadoCoeficientes[i] = 0.0;
+        resultadoCoeficientes[i] = 0;
         resultadoExpoentes[i] = i;
     }
 
@@ -153,12 +153,12 @@ void somarPolinomios(double coeficientes1[], int expoentes1[], int grau1, double
     }
 }
 
-void multiplicarPolinomios(double coeficientes1[], int expoentes1[], int grau1, double coeficientes2[], int expoentes2[], int grau2, double resultadoCoeficientes[], int resultadoExpoentes[], int *resultadoGrau) {
+void multiplicarPolinomios(int coeficientes1[], int expoentes1[], int grau1, int coeficientes2[], int expoentes2[], int grau2, int resultadoCoeficientes[], int resultadoExpoentes[], int *resultadoGrau) {
     int i, j;
     *resultadoGrau = grau1 + grau2;
 
     for (i = 0; i <= *resultadoGrau; i++) {
-        resultadoCoeficientes[i] = 0.0;
+        resultadoCoeficientes[i] = 0;                 
         resultadoExpoentes[i] = i;
     }
 
@@ -168,4 +168,3 @@ void multiplicarPolinomios(double coeficientes1[], int expoentes1[], int grau1, 
         }
     }
 }
-
